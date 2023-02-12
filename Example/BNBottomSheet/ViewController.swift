fc3e270c5b3bdfaff8598fedf0f1cf2d35d6a6ed
botirjon <botirjon.nasridinov@gmail.com>
2023-02-12 17:49:00 +0500

    RELEASE 1.1.1

--- Example/BNBottomSheet/ViewController.swift
+++ Example/BNBottomSheet/ViewController.swift
@@ -7,18 +7,71 @@
 //
 
 import UIKit
+import BNBottomSheet
+import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Open", for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: button.superview!.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: button.superview!.centerYAnchor)
+        ])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc private func buttonTapped() {
+        let bottomSheet = BNBottomSheetController()
+        bottomSheet.dataSource = self
+        bottomSheet.delegate = self
+        if #available(iOS 13.0, *) {
+            bottomSheet.styles.contentBackgroundColor = .init(dynamicProvider: { traitCollection in
+                return traitCollection.userInterfaceStyle == .dark ? .white : .gray
+            })
+        } else {
+            bottomSheet.styles.contentBackgroundColor = .green
+        }
+        present(bottomSheet, animated: true)
     }
-
+    
+    private let items: [String] = [
+        "Apple",
+        "Orange",
+        "Banana"
+    ]
 }
 
+
+extension ViewController: BNBottomSheetDataSource {
+    func numberOfSections(in bottomSheetController: BNBottomSheetController) -> Int {
+        return 1
+    }
+    
+    func bottomSheetController(_ controller: BNBottomSheetController, numberOfItemsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    func bottomSheetController(_ controller: BNBottomSheetController, cellForItemAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = items[indexPath.row]
+        cell.textLabel?.textColor = .systemBlue
+        cell.textLabel?.font = .systemFont(ofSize: 24, weight: .semibold)
+        return cell
+    }
+}
+
+extension ViewController: BNBottomSheetDelegate {
+    func bottomSheetController(_ controller: BNBottomSheetController, heightForItemAt indexPath: IndexPath) -> CGFloat {
+        max(UITableView.automaticDimension, 46)
+    }
+}
